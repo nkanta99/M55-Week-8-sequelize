fade5d83a72f0905f8ae08f3ff77a06f4ba1e52c
isaac nkanta <170721134+nkanta99@users.noreply.github.com>
2024-07-09 12:21:56 +0100

    feature: add Books model

--- src/server.js
+++ src/server.js
@@ -3,14 +3,21 @@ const express = require("express");
 
 const port = process.env.PORT || 5001;
 
+const Book = require("./books/model");
+
 const app = express()
 
 app.use(express.json());
+
+const syncTables = () => {
+    Book.sync();
+};
 
 app.get("/health", (req, res) => {
     res.status(200).json({message: "API is healthy"});
 });
 
 app.listen(port, () => {
+    syncTables();
     console.log(`server is listening on port ${port}`);
 });
