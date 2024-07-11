23a5e79f24ffd7265f133e04cb59c77f84eabe98
isaac nkanta <170721134+nkanta99@users.noreply.github.com>
2024-07-11 14:12:05 +0100

    feature: add update and delete routes

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -27,7 +27,42 @@ const getAllBooks = async (req, res) => {
     }
 };
 
+const updateBook = async (req, res) => {
+  try {
+    const books = await Book.update();
+
+    res.status(202).json({ message: "success", books: books });
+  } catch (error) {
+    res.status(501).json({ message: error.message, error: error });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const books = await Book.delete();
+
+    res.status(204).json({ message: "success", books: books });
+  } catch (error) {
+    res.status(501).json({ message: error.message, error: error });
+  }
+};
+
+const getBookByTitle = async (req, res) => {
+    try {
+        console.log("req.params:", req.params);
+        const book = await Book.findOne({where: { title: req.params.title } });
+        res
+        .status(200)
+        .json({message: "success", book: book, params: req.params});
+    } catch (error) {
+        res.status(501).json({message: error.message, error: error}); 
+    }
+};
+
 module.exports = {
     addBook: addBook,
     getAllBooks: getAllBooks,
+    updateBook: updateBook,
+    deleteBook: deleteBook,
+    getBookByTitle: getBookByTitle,
 };
--- src/books/routes.js
+++ src/books/routes.js
@@ -1,7 +1,7 @@
 const {Router} = require("express");
 const bookRouter = Router();
 
-const {addBook, getAllBooks} = require("./controllers");
+const {addBook, getAllBooks, updateBook, deleteBook, getBookByTitle} = require("./controllers");
 
 // http://localhost:5001/books/test
 bookRouter.get("/test", async (req, res) => {
@@ -13,6 +13,16 @@ bookRouter.get("/awesome", async (req, res) => {
     res.status(200).json({message: "test a ok!" });
 });
 
+// http://localhost:5001/books/update
+bookRouter.put("/update", async (req, res) => {
+    res.status(202).json({message: "added update" });
+});
+
+// http://localhost:5001/books/done
+bookRouter.delete("/done", async (req, res) => {
+    res.status(204).json({message: "deleted done" });
+});
+
 // // http://localhost:5001/books/books
 // bookRouter.get("/books", async (req, res) => {
 //     res.status(200).json({message: "book added" });
@@ -22,6 +32,12 @@ bookRouter.get("/awesome", async (req, res) => {
 bookRouter.post("/addBook", addBook);
 
 bookRouter.get("/getAllBooks", getAllBooks);
+
+bookRouter.put("/updateBook", updateBook);
+
+bookRouter.delete("/deleteBook", deleteBook);
+
+bookRouter.get("/getBookByTitle/:title", getBookByTitle);
 
 
 
